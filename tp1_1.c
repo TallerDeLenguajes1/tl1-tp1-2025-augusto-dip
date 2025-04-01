78b451e6d9bcdae0e48ebf7c65c06746aadbc371
augusto-dip <augustodip26@gmail.com>
2025-04-01 20:48:22 -0300

    segundo commit de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main (){
-    printf("Hola mundo");
-    getchar();
+    float nota;
+    float *p_nota;
 
-return 0;
+    nota = 9.12;
+    p_nota = &nota;
+
+    printf("Valor de nota: %f\n", *p_nota);  
+    printf("Direccion de memoria: %p\n", p_nota); 
+    printf("Dirección de memoria de la variable: %p\n", &nota);  
+    printf("Dirección de memoria del puntero.: %p\n", &p_nota);  
+    printf("Tamaño de memoria utilizado por la variable: %d bytes\n",(int)sizeof(nota));
+    printf("\nHola mundo\n");
+    getchar(); 
+    return 0;
 }
